54dac2e901352098ec45ed95d297a85fce0a57dd
ACER <ACER@DESKTOP-TRKNRSV>
2021-05-10 13:15:45 +0300

    Add project files.

---
+++ MvcProjeKampi1/Controllers/TextController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi1.Controllers
+{
+    public class TextController : Controller
+    {
+        // GET: Text
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Text2()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcProjeKampi1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcProjeKampi1.MvcApplication" Language="C#" %>
